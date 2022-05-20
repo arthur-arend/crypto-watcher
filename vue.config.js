a59c3282236ffad2bbe32f0f2d7528a7904b4587
Arthur Arend <arthur.nox@gmail.com>
2022-05-20 05:37:33 -0300

    Config

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  publicPath: '/crypto-watcher/',
+
+  
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/crypto-watcher/'
+    : '/',
 
   // devServer: {
   //   proxy: 'http://localhost:4000'
